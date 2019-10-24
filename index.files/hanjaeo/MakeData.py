2e4966c96906b92a531722d1e8e611651cb86435
sgalal <sgalal.me@outlook.com>
2019-10-24 17:38:09 +0800

    Update link

--- index.files/hanjaeo/MakeData.py
+++ index.files/hanjaeo/MakeData.py
@@ -1,11 +1,12 @@
 #!/usr/bin/env python3
+# -*- coding: utf-8 -*-
 
-import urllib.request
-import re
 from collections import defaultdict
 import json
+import re
+import urllib.request
 
-source_url = 'https://raw.githubusercontent.com/suginacc/sugina/943741f8417e20ec15256aadcd6254009d13127b/config/hanja.txt'
+source_url = 'https://raw.githubusercontent.com/sugina-dev/sugina/943741f8417e20ec15256aadcd6254009d13127b/config/hanja.txt'
 d = defaultdict(list)
 for line in re.finditer(r'^([^ ]{2,}) (.+)$', urllib.request.urlopen(source_url).read().decode('utf-8'), re.MULTILINE):
     word = line[1]
